d60b3c64a54efd9270dd53f4eb6f89e88aa51fab
Annie <annielin28815@gmail.com>
2024-01-01 22:51:53 +0800

    add btn list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [btnList, setBtnList] = useState([
+    {
+      id: 1,
+      name: "主站",
+      url: ""
+    },
+    {
+      id: 2,
+      name: "Portal",
+      url: ""
+    },
+    {
+      id: 3,
+      name: "Intranet(新)",
+      url: ""
+    },
+    {
+      id: 4,
+      name: "Intranet(舊)",
+      url: ""
+    },
+    {
+      id: 5,
+      name: "供應鏈(新)",
+      url: ""
+    },
+    {
+      id: 6,
+      name: "供應鏈(舊)",
+      url: ""
+    },
+  ]);
+
+  const openTabs = (url) => {
+    window.open(url, "_blank");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Choose it!</h3>
+      <div className="btn-list">
+        {btnList.length > 0 && btnList.map((item) => {
+          return (
+            <button onClick={() => openTabs(item.url)}>{item.name}</button>
+          )
+        })}
+      </div>
     </div>
   );
 }
