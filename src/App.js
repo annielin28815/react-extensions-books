112a0605667d49e009565a116535aae90f605573
Annie <annielin28815@gmail.com>
2024-01-03 23:27:06 +0800

    add mail link and reflactor style

--- src/App.js
+++ src/App.js
@@ -40,14 +40,30 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h3>Choose it!</h3>
+    <div className="app">
+      <h3>Shortcut key</h3>
       <div className="btn-list">
         {btnList.length > 0 && btnList.map((item) => {
           return (
-            <button onClick={() => openTabs(item.url)} className='btn'>{item.name}</button>
+            <button key={item.id} className="btn" onClick={() => openTabs(item.url)}>
+              <span className="deco"></span>
+              <span className="text">{item.name}</span>
+            </button>
           )
         })}
+      </div>
+
+      <div className="mail-btn-list">
+        <span>一鍵寄信給老大</span>
+        <a href="mailto:paul_chan@books.com.tw" className="mail-link">
+          <span className="deco">
+            <svg className="deco-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+            </svg>
+          </span>
+          <span className="text" title="open mail server">go</span>
+          <span className="invisible" title="open mail server">go</span>
+        </a>
       </div>
     </div>
   );
